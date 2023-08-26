bcc29106109ec61d605f9f6675bfbc8caea238f6
ziu7wx <jin.zhu5@cn.bosch.com>
2023-08-27 07:16:53 +0800

    feat: update

---
+++ python/designer_door_mat.py
@@ -0,0 +1,15 @@
+def door_mat(row, col) -> str:
+    for r in range(row):
+        if r == (row - 1) / 2:
+            center = "WELCOME"
+        else:
+            if r > row / 2:
+                r = row - r -1 
+            center = (2 * r + 1) * (".|.")
+        lpad = rpad = int((col - len(center)) / 2) * "-"
+        res = lpad + center + rpad
+        print(res)
+
+if __name__ == '__main__':
+    row, col = input().split()
+    door_mat(int(row), int(col))
---
+++ python/text_wraper.py
@@ -0,0 +1,19 @@
+import textwrap
+
+def wrap(string: str, max_width: int) -> list:
+    # res = textwrap.wrap(string, max_width)
+    res = []
+    substring = ''
+    for i, s in enumerate(string):
+        substring += s
+        if len(substring) == max_width:
+            res.append(substring)
+            substring = ''
+    if substring:
+        res.append(substring)
+    return "\n".join(res)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
