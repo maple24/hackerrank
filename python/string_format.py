26e5bd3fe4d69977b3d59d71923ffe05805b1bcf
ziu7wx <jin.zhu5@cn.bosch.com>
2023-08-27 13:43:38 +0800

    feat: update

---
+++ python/string_format.py
@@ -0,0 +1,6 @@
+def print_formatted(number):
+    for i in range(1, number+1):
+        pad = len(bin(number)[2:])
+        print(f"{i: >{pad}} {oct(i)[2:]: >{pad}} {hex(i)[2:].upper(): >{pad}} {bin(i)[2:]: >{pad}}")
+
+print_formatted(63)
