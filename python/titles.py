4a9b20cd4cfd25c0286da2005f9af9aa40c96358
ziu7wx <jin.zhu5@cn.bosch.com>
2023-08-27 21:31:39 +0800

    feat: update

---
+++ python/titles.py
@@ -0,0 +1,15 @@
+a = '23hello    23world    lol    2g'
+import re
+def solve(s: str):
+    words = re.split(r'(\W+)', s)
+    result = []
+    
+    for word in words:
+        restored_word = word
+        if re.match(r'\w+', word):
+            restored_word = word.capitalize()
+        result.append(restored_word)
+    
+    return ''.join(result)
+
+print(solve(a))
